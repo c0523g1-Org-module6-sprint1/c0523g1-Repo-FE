c4e01e17d92a8a51b18b2493a15adf77f7d41c49
hungcg <venenozelo94@gmail>
2023-11-16 11:21:19 +0700

    commitHungHLP

--- eros05/src/components/invited_recommend_friend/RecommendList.js
+++ eros05/src/components/invited_recommend_friend/RecommendList.js
@@ -18,23 +18,38 @@ export default function RecommendList() {
     if (!recommend) return null;
     return (
         <>
-            <h1>Lời mời kết bạn</h1>
-            recommend.map((recommend) => {
-            <div className="d-flex" style="float: left; margin:0 32px">
-                <div className="cards">
-                    <div className="icon">
-                        <img className="cus-avatar"
-                             src="${recommend.image}"
-                             alt=""/>
-                    </div>
-                    <p className="title mb-4">${recommend.name}</p>
-                    <h6>${recommend.mutualFriends + " bạn chung"}</h6>
-                    <p className="text">
-                        <button id="confirm" className="btn mt-5 ">Xem trang cá nhân</button>
-                    </p>
+            <div>
+                <h1>Lời mời kết bạn</h1>
+                <div className="dropdown" style={{margin: "0 10px"}}>
+                    <button className="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                        Type
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li className="dropdown-item" >Theo lời mời mới nhất</li>
+                        <li className="dropdown-item">Theo lời mời cũ nhất</li>
+                    </ul>
                 </div>
+                <button className="btn btn-success" type="submit">Reset</button>
             </div>
-        })
+            {recommend.map((recommend) =>
+                <div className="d-flex">
+                    <div className="cards">
+                        <div className="icon">
+                            <img className="cus-avatar"
+                                 src={recommend.avatar}
+                                 alt=""/>
+                        </div>
+                        <p className="title mb-4">${recommend.name}</p>
+                        <p className="text">
+                            <button id="confirm" className="btn ">Xác nhận</button>
+                            <button id="delete" className="btn text-dark mt-1"
+                                    type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Xóa
+                            </button>
+                        </p>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
