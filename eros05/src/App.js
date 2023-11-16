cbe34787ec543c96a8109e4be8b80a41e6766c30
quy116 <nguyenphucquy1106@gmail.com>
2023-11-16 21:39:11 +0700

    [Gift][QuyNP][create password]

--- eros05/src/App.js
+++ eros05/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import {ToastContainer} from "react-bootstrap";
-import 'react-toastify/dist/ReactToastify.css';
-import {Chatbox} from "./components/chatbox/Chatbox";
+import "./App.css";
+import { ToastContainer } from "react-bootstrap";
+import "react-toastify/dist/ReactToastify.css";
+import { Chatbox } from "./components/chatbox/Chatbox";
 import Header from "./components/header/Header";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+import Changepassword from "./components/changePassword/Changepassword";
 function App() {
   return (
-      <>
-        <ToastContainer/>
-          <Chatbox/>
-          <Header/>
-      </>
+    <>
+      <ToastContainer />
+      <Chatbox />
+      <Header />
+    </>
   );
 }
 export default App;
--- eros05/src/service/chatbox/apiConnection.js
+++ eros05/src/service/chatbox/apiConnection.js
@@ -1,40 +1,47 @@
 import axios from "axios";
 
 export const GetProfileApi = async () => {
-    try {
-        const res = await axios.get(`http://localhost:8080/profile`)
-        return res;
-    } catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    const res = await axios.get(`http://localhost:8080/profile`);
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
 export const GetFriendsApi = async (name) => {
-    try {
-        const res = await axios.get(`http://localhost:8080/friends?name_like=${name}`)
-        return res.data;
-    } catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    const res = await axios.get(
+      `http://localhost:8080/friends?name_like=${name}`
+    );
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 export const GetUnknowApi = async (name) => {
-    try {
-        const res = await axios.get(`http://localhost:8080/unknow?name_like=${name}`)
-        return res.data;
-    } catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    const res = await axios.get(
+      `http://localhost:8080/unknow?name_like=${name}`
+    );
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 export const GetChatBoxApi = async (id1, id2) => {
-    try {
-        const res1 = await axios.get(`http://localhost:8080/mess?sender=${id1}&recipient=${id2}`);
-        const res2 = await axios.get(`http://localhost:8080/mess?sender=${id2}&recipient=${id1}`);
+  try {
+    const res1 = await axios.get(
+      `http://localhost:8080/mess?sender=${id1}&recipient=${id2}`
+    );
+    const res2 = await axios.get(
+      `http://localhost:8080/mess?sender=${id2}&recipient=${id1}`
+    );
 
-        let res = [...res1.data, ...res2.data];
-        res.sort((a,b) => new Date(b.releaseTime) - new Date(a.releaseTime));
+    let res = [...res1.data, ...res2.data];
+    res.sort((a, b) => new Date(b.releaseTime) - new Date(a.releaseTime));
 
-        return res;
-    } catch (e) {
-        console.log(e);
-    }
-
-}
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
