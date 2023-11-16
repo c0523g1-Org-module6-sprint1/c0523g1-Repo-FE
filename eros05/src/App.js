7d8024ddb051f610cc6872627a5aa8205cbcae77
quy116 <nguyenphucquy1106@gmail.com>
2023-11-16 08:53:33 +0700

    hi

--- eros05/src/App.js
+++ eros05/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import {ToastContainer} from "react-bootstrap";
-import 'react-toastify/dist/ReactToastify.css';
-import {Chatbox} from "./components/chatbox/Chatbox";
+import "./App.css";
+import { ToastContainer } from "react-bootstrap";
+import "react-toastify/dist/ReactToastify.css";
+import { Chatbox } from "./components/chatbox/Chatbox";
+
 function App() {
   return (
-      <>
-        <ToastContainer/>
-          <Chatbox/>
-      </>
+    <>
+      <ToastContainer />
+      <Chatbox />
+    </>
   );
 }
-
 export default App;
