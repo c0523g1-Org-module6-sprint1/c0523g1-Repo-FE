ee13227e98f4c411a98e2abf93bf4a9af2b4047a
phanquocsang97 <quocsang0310@gmail.com>
2023-11-17 10:15:08 +0700

    check

--- eros05/src/components/bodyMainPage/BodyMainPage.js
+++ eros05/src/components/bodyMainPage/BodyMainPage.js
@@ -8,7 +8,7 @@ import {useNavigate} from "react-router-dom";
 const BodyMainPage = () => {
     const navigate = useNavigate();
     const goRegisterPage = ()=>{
-        navigate(`Sang`)
+        navigate(`register`)
     }
     return (
         <section className="body-main-page">
