6f2e972dd1fd8462c0c3fd111999f9607d069f37
dinhlong1999 <long.truong.vcr@gmail.com>
2023-11-17 08:36:34 +0700

    [longtnd][personalpage]

--- eros05/src/service/personalPage/PersonalpageService.js
+++ eros05/src/service/personalPage/PersonalpageService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const URL_PERSONAL_PAGE = "http://localhost:8080/api/personal-page/"
-const URL = "http://localhost:8080/"
+const URL_PERSONAL_PAGE = "http://localhost:8080/api/public/personal-page/"
+const URL = "http://localhost:8080//api/public/"
+const URL_SENT_INVITE = "http://localhost:8080/api/public/sent-invite/"
+const URL_GET_STATUS ="http://localhost:8080/api/public/status/"
 
 export const getInfoPersonal = async (id) => {
    try {
@@ -19,3 +21,18 @@ export const getInfoAccount = async (name) =>{
     }
 }
 
+export const sentInvite = async (relationships) => {
+    try {
+        return await axios.post(URL_SENT_INVITE,relationships)
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+export const getStatus = async (idSent, idReceiver) =>{
+    try {
+       return  await axios.get(URL_GET_STATUS+`${idSent}/${idReceiver}`)
+    }catch (e) {
+        console.log(e.message)
+    }
+}
