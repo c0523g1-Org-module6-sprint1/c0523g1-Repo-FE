fb9fcb7fd6dbb5430380842bd21245448bf8c26f
bondatfpt <bondatfpt@gmail.com>
2023-11-20 08:23:28 +0700

    post-service

--- eros05/src/service/posts/PostService.js
+++ eros05/src/service/posts/PostService.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-import { async } from "q";
 export const getListNewsfeed = async (loggedInAccountId) => {
     try {
         const respone = await axios.get("http://localhost:8080/api/public/post/newsfeed/" +loggedInAccountId);
-        console.log(respone.data);
         return respone.data; 
     } catch (error) {
         console.log(error);
@@ -13,7 +11,6 @@ export const getListNewsfeed = async (loggedInAccountId) => {
 export const getPostById = async (id) => {
     try {
         const respone = await axios.get("http://localhost:8080/api/public/newsfeed/post/" + id);
-        console.log(respone.data);
         return respone.data; 
     } catch (error) {
         console.log(error);
@@ -23,7 +20,6 @@ export const getPostById = async (id) => {
 export const getPrivacyPost = async () => {
     try {
         const respone = await axios.get("http://localhost:8080/api/public/privacy-post");
-        console.log(respone.data);
         return respone.data; 
     } catch (error) {
         console.log(error);
@@ -32,7 +28,7 @@ export const getPrivacyPost = async () => {
 
 export const update = async (id,values) => {
     try {
-        const respone = await axios.patch("http://localhost:8080/api/public/newsfeed/post/admin/" + id,values);
+        const respone = await axios.patch("http://localhost:8080/api/public/post/admin/" + id,values);
         console.log(respone.status);
         return respone.status; 
     } catch (error) {
